80e00668a8ae6e165b3e3fb7e34df03792a01c22
Krishna Pala <palakrishnasai@gmail.com>
2020-07-18 11:57:46 +0530

    Add files via upload

---
+++ Substrings-of-size-K-with-K-distinct-chars.java
@@ -0,0 +1,24 @@
+public class Main {
+    
+    public static List<String> kSubstring(String s, int k) {
+        Set<Character> window = new HashSet<>();
+        Set<String> result = new HashSet<>();
+        for (int start = 0, end = 0; end < s.length(); end++) {
+            for (; window.contains(s.charAt(end)); start++) {
+                window.remove(s.charAt(start));
+            }
+
+            window.add(s.charAt(end));
+
+            if (window.size() == k) {
+                result.add(s.substring(start, end + 1));
+                window.remove(s.charAt(start++));
+            }
+        }
+        return new ArrayList<>(result);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(kSubstring("krishnasaipala", 4));
+    }
+}
